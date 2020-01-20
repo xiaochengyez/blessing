8c2eb98707fd2063638aab371b5736e30864723b
gongxiaocheng <gxcsgdhr@outlook.com>
2020-01-20 16:31:15 +0800

    one

---
+++ deploy_docker.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+. /etc/profile
+
+cd $PROJ_PATH
+mvn clean install
+
+#准备ROOT.war包
+cd $PROJ_PATH/target
+mv blessing.war ROOT.war
+
+#制作新的docker image - iweb
+cd $PROJ_PATH
+docker stop blessing
+docker rm blessing
+docker rmi blessing
+docker build -t blessing .
+
+#启动docker image, 宿主机暴露端口 8111
+docker run --name myblessing -d -p 8112:8080 blessing
+
+
+
